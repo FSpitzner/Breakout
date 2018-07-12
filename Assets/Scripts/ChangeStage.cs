4f25c76e2b55add725c31bc541e77fd250f17f0e
FSpitzner <38406798+FSpitzner@users.noreply.github.com>
2018-07-12 17:44:55 +0200

    Türverriegelung gefixt

--- Assets/Scripts/ChangeStage.cs
+++ Assets/Scripts/ChangeStage.cs
@@ -39,7 +39,8 @@ public class ChangeStage : InteractTrigger {
 
 	public override void Interact(){
         Debug.Log("Interacting");
-		if (!door.tweening && door.CheckDoorIsLocked()) {
+		if (!door.tweening && !door.CheckDoorIsLocked()) {
+            Debug.Log("Hier bin ich");
             PlayerController player = PlayerController.instance;
             foreach(GameObject g in room1Objects)
             {
