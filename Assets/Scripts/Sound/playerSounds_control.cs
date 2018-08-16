cdd1a7c7a33af1426e22b69a2d5df9da9ed3b49d
Der Flip <bruchpilot747@gmail.com>
2018-08-16 11:10:05 +0200

    ambient rain, ambient wind, other minor stuff
    @Friedemann: du musst den wind und das gewitter bei dir im Event Emitter neu verlinken: lupe>weather

--- Assets/Scripts/Sound/playerSounds_control.cs
+++ Assets/Scripts/Sound/playerSounds_control.cs
@@ -36,37 +36,37 @@ public class playerSounds_control : MonoBehaviour
     {
         if (this.fear.fear > fear.getFearStart() && fear.fear < fear.getFearLow() - 0.01)
         {
-            eventEmitterRefHeartBeat.SetParameter("speed", 0f);
+            eventEmitterRefHeartBeat.SetParameter(Constants.HEARTSPEED, 0f);
             Debug.Log("heartbeat Speed VLOW");
         }
         else if (fear.fear >= fear.getFearLow() && fear.fear < fear.getFearLowMedium() - 0.01)
         {
-            eventEmitterRefHeartBeat.SetParameter("speed", 1f);
+            eventEmitterRefHeartBeat.SetParameter(Constants.HEARTSPEED, 1f);
             Debug.Log("heartbeat Speed LOW");
         }
         else if (fear.fear >= fear.getFearLowMedium() && fear.fear < fear.getFearMedium()-0.01)
         {
-            eventEmitterRefHeartBeat.SetParameter("speed", 2f);
+            eventEmitterRefHeartBeat.SetParameter(Constants.HEARTSPEED, 2f);
             Debug.Log("heartbeat Speed LMEDIUM");
         }
         else if (fear.fear >= fear.getFearMedium() && fear.fear < fear.getFearHighMedium() - 0.01)
         {
-            eventEmitterRefHeartBeat.SetParameter("speed", 3f);
+            eventEmitterRefHeartBeat.SetParameter(Constants.HEARTSPEED, 3f);
             Debug.Log("heartbeat Speed MEDIUM");
         }
         else if (fear.fear >= fear.getFearHighMedium() && fear.fear < fear.getFearHigh() - 0.01)
         {
-            eventEmitterRefHeartBeat.SetParameter("speed", 4f);
+            eventEmitterRefHeartBeat.SetParameter(Constants.HEARTSPEED, 4f);
             Debug.Log("heartbeat Speed HMEDIUM");
         }
         else if (fear.fear >= fear.getFearHigh() && fear.fear < fear.getFearPanic() - 0.01)
         {
-            eventEmitterRefHeartBeat.SetParameter("speed", 5f);
+            eventEmitterRefHeartBeat.SetParameter(Constants.HEARTSPEED, 5f);
             Debug.Log("heartbeat Speed HIGH");
         }
         else if (fear.fear >= fear.getFearPanic())
         {
-            eventEmitterRefHeartBeat.SetParameter("speed", 6f);
+            eventEmitterRefHeartBeat.SetParameter(Constants.HEARTSPEED, 6f);
             Debug.Log("heartbeat Speed VHIGH");
         }
         eventEmitterRefHeartBeat.Play();
--- Assets/Scripts/Utility/Constants.cs
+++ Assets/Scripts/Utility/Constants.cs
@@ -20,8 +20,15 @@ public class Constants{
     #endregion
 
     #region FMOD Parameter Names
+
     public static readonly string THUNDERID = "ThunderID2Play";
     public static readonly string SOUNDSTATE = "InXOut";
+
+    public static readonly string AMBWINDVOL = "ambwindvol";
+    public static readonly string HEARTVOL = "heartvolume";
+    public static readonly string AMBRAINVOL = "ambrainvol";
+
+    public static readonly string HEARTSPEED = "heartspeed";
 
     #endregion
 }
