ea712ac50c893f7b9ce4a16ee7f66ec7e6c4f31b
Joghurtdressing <schaefer483@googlemail.com>
2018-08-27 16:55:43 +0200

    AnimationStairs

---
+++ Assets/Scripts/Utility/AnimatorBoolSetter.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class AnimatorBoolSetter : MonoBehaviour {
+
+    public Animator animator;
+
+    public void SetBoolTrue(string name)
+    {
+        animator.SetBool(name, true);
+    }
+}
