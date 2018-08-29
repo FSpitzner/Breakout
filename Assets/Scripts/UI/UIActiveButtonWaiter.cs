bca49ae488bef80bcf3713ee26ab9637a23d82d9
FSpitzner <38406798+FSpitzner@users.noreply.github.com>
2018-08-29 16:06:42 +0200

    Credits + UI-Update + Stairs Bugfix + Dreamworld Objects Delay

---
+++ Assets/Scripts/UI/UIActiveButtonWaiter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIActiveButtonWaiter : MonoBehaviour {
+
+    public GameObject acitveObject;
+    public EventSystem eventSystem;
+    
+    public void DelayedSelection(Object obj)
+    {
+        eventSystem.SetSelectedGameObject(acitveObject);
+    }
+}
